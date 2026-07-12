6c11475f7cfb140455d97895fd8c754e2f749c53
Lakshaygaur391 <rajivshukla6342@gmail.com>
2026-07-12 12:20:56 +0530

    Add Firebase environment files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDgJmv9-wtR7yOr0yDb2ncENKo2vMOabQY",
+    authDomain: "portfolio-website-c93fd.firebaseapp.com",
+    projectId: "portfolio-website-c93fd",
+    storageBucket: "portfolio-website-c93fd.firebasestorage.app",
+    messagingSenderId: "200446692385",
+    appId: "1:200446692385:web:5bba304fe94a4cd61d6760",
+    measurementId: "G-8PE24QLRR8"
+  }
+};
